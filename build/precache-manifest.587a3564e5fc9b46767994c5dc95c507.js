3023aea48513ffa9b1b470ebed1d1fc317d69bb1
Viktor Nareiko <viktor.nareiko@adaptagency.com>
2019-06-05 04:33:37 +0300

    Using root folder for github pages

---
+++ build/precache-manifest.587a3564e5fc9b46767994c5dc95c507.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44fa39ff0eeb2ece5f50e10e11758e1",
+    "url": "/ultra-wizard/index.html"
+  },
+  {
+    "revision": "72540c27b94e31aea4e2",
+    "url": "/ultra-wizard/static/css/main.5144918f.chunk.css"
+  },
+  {
+    "revision": "2da3b42876082feab3fc",
+    "url": "/ultra-wizard/static/js/2.dfc9ae99.chunk.js"
+  },
+  {
+    "revision": "72540c27b94e31aea4e2",
+    "url": "/ultra-wizard/static/js/main.fce21965.chunk.js"
+  },
+  {
+    "revision": "0629bed6aea89895d882",
+    "url": "/ultra-wizard/static/js/runtime~main.5059387f.js"
+  },
+  {
+    "revision": "745ab4ede44e4c88aa8dda40e1115ad3",
+    "url": "/ultra-wizard/static/media/dot--grey.745ab4ed.svg"
+  },
+  {
+    "revision": "bff5b07ccbc3c7422b2b060c7a2bb058",
+    "url": "/ultra-wizard/static/media/dot--red.bff5b07c.svg"
+  },
+  {
+    "revision": "bf289384f98b1859dc4d5438e8d2f8ae",
+    "url": "/ultra-wizard/static/media/money-bag--grey.bf289384.svg"
+  },
+  {
+    "revision": "d351e608f7b694b053fc26b8bb6f72f9",
+    "url": "/ultra-wizard/static/media/money-bag--red.d351e608.svg"
+  }
+]);
