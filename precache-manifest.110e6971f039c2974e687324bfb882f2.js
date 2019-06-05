84028f0c3bfdce7cb806d902bd41a74d1d5903ad
Viktor Nareiko <viktor.nareiko@adaptagency.com>
2019-06-05 04:29:48 +0300

    Updates

---
+++ precache-manifest.110e6971f039c2974e687324bfb882f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3388ed53bfc9402540cfc065313f8aa",
+    "url": "/ultra-wizard/build/index.html"
+  },
+  {
+    "revision": "6d23adccf71ade458911",
+    "url": "/ultra-wizard/build/static/css/main.97175bd5.chunk.css"
+  },
+  {
+    "revision": "2da3b42876082feab3fc",
+    "url": "/ultra-wizard/build/static/js/2.dfc9ae99.chunk.js"
+  },
+  {
+    "revision": "6d23adccf71ade458911",
+    "url": "/ultra-wizard/build/static/js/main.fce21965.chunk.js"
+  },
+  {
+    "revision": "40f0bb753e4fb31b2796",
+    "url": "/ultra-wizard/build/static/js/runtime~main.af62bac7.js"
+  },
+  {
+    "revision": "745ab4ede44e4c88aa8dda40e1115ad3",
+    "url": "/ultra-wizard/build/static/media/dot--grey.745ab4ed.svg"
+  },
+  {
+    "revision": "bff5b07ccbc3c7422b2b060c7a2bb058",
+    "url": "/ultra-wizard/build/static/media/dot--red.bff5b07c.svg"
+  },
+  {
+    "revision": "bf289384f98b1859dc4d5438e8d2f8ae",
+    "url": "/ultra-wizard/build/static/media/money-bag--grey.bf289384.svg"
+  },
+  {
+    "revision": "d351e608f7b694b053fc26b8bb6f72f9",
+    "url": "/ultra-wizard/build/static/media/money-bag--red.d351e608.svg"
+  }
+]);
