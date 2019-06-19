05bf9aca585c5f42f489e2134ddc53d65ef6a8c2
Viktor Nareiko <viktor.nareiko@adapt.dk>
2019-06-19 09:24:56 +0300

    Added redux dev tools for redux store debugging.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,10 @@
 import { createStore } from 'redux';
-import rootReducer from './reducers/index';
+import rootReducer from './reducers';
 
-export default createStore(rootReducer);
+console.log(process.env.ENVIRONMENT);
+
+export default createStore(
+  rootReducer,
+  process.env.NODE_ENV === 'development'
+    && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
