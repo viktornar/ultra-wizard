f312a54e1aa67e150c2f75caa5d6b87122eec3a2
Viktor Nareiko <viktor.nareiko@adapt.dk>
2019-06-19 12:18:13 +0300

    Fixed broken test.

--- src/WizardApp.test.js
+++ src/WizardApp.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
 import WizardApp from './WizardApp';
-import {wizardConfig} from "./config";
+import { wizardConfig } from './config';
+import {initialState} from './redux/reducers/wizard';
 
 it('renders without crashing', () => {
+
+  const mockStore = configureMockStore();
+  const store = mockStore({ wizard: initialState });
+
   const div = document.createElement('div');
-  ReactDOM.render(<WizardApp wizardConfig={wizardConfig}/>, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <WizardApp wizardConfig={wizardConfig}/>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
